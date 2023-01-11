f66fdbab323c2704ba9b75394e256514faf4a9d2
Waheed Afolabi <waheed.remote247@gmail.com>
2023-01-11 17:27:08 +0100

    Add basic structure for the main view

---
+++ TipCalculator/Screen/MainViewController.swift
@@ -0,0 +1,74 @@
+//
+//  MainViewController.swift
+//  TipCalculator
+//
+//  Created by waheedCodes on 11/01/2023.
+//
+
+import UIKit
+import SnapKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let logoView = LogoView()
+    private let resultView = ResultView()
+    private let billInputView = BillInputView()
+    private let tipInputView = TipInputView()
+    private let splitTipInputView = SplitTipInputView()
+    
+    private lazy var vStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            logoView,
+            resultView,
+            billInputView,
+            tipInputView,
+            splitTipInputView
+        ])
+        stackView.axis = .vertical
+        stackView.spacing = 36
+        return stackView
+    }()
+
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        layoutSubviews()
+    }
+    
+    // MARK: - Helper Methods
+    
+    private func layoutSubviews() {
+        view.addSubview(vStackView)
+        
+        vStackView.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.topMargin).offset(16)
+            make.trailing.equalTo(view.snp.trailingMargin).offset(-16)
+            make.bottom.equalTo(view.snp.bottomMargin).offset(-16)
+            make.leading.equalTo(view.snp.leadingMargin).offset(16)
+        }
+        
+        logoView.snp.makeConstraints { make in
+            make.height.equalTo(48)
+        }
+        
+        resultView.snp.makeConstraints { make in
+            make.height.equalTo(224)
+        }
+        
+        billInputView.snp.makeConstraints { make in
+            make.height.equalTo(56)
+        }
+        
+        tipInputView.snp.makeConstraints { make in
+            make.height.equalTo(56+16+56)
+        }
+        
+        splitTipInputView.snp.makeConstraints { make in
+            make.height.equalTo(56)
+        }
+    }
+}
---
+++ TipCalculator/Screen/Views/SplitTipInputView.swift
@@ -0,0 +1,28 @@
+//
+//  SplitTipInputView.swift
+//  TipCalculator
+//
+//  Created by waheedCodes on 11/01/2023.
+//
+
+import UIKit
+
+class SplitTipInputView: UIView {
+
+    // MARK: - Initializers
+    
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configuration
+    
+    private func layout() {
+        backgroundColor = .magenta
+    }
+}
