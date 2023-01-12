cb1b6c60b418151abb9db436e601c3f7f78686ca
Waheed Afolabi <waheed.remote247@gmail.com>
2023-01-12 10:19:09 +0100

    Add custom app fontas

---
+++ TipCalculator/Config/ThemeFont.swift
@@ -0,0 +1,23 @@
+//
+//  ThemeFont.swift
+//  TipCalculator
+//
+//  Created by waheedCodes on 12/01/2023.
+//
+
+import UIKit
+
+struct ThemeFont {
+    
+    static func regular(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Regular", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func bold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Bold", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func demiBold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-DemiBold", size: size) ?? .systemFont(ofSize: size)
+    }
+}
