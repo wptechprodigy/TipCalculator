847cda6f1cb3f968e312d4fc47cd18f2dc510173
Waheed Afolabi <waheed.remote247@gmail.com>
2023-01-12 10:18:48 +0100

    Add custom app theme of colors

---
+++ TipCalculator/Config/ThemeColor.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeColor.swift
+//  TipCalculator
+//
+//  Created by waheedCodes on 12/01/2023.
+//
+
+import UIKit
+
+struct ThemeColor {
+    
+    static let bg          =    UIColor(hexString: "F5F3F4")
+    static let primary     =    UIColor(hexString: "1CC9BE")
+    static let secondary   =    UIColor.systemOrange
+    static let text        =    UIColor(hexString: "000000")
+    static let separator   =    UIColor(hexString: "CCCCCC")
+}
